6a865ff7f8bd6dd1052ff4a072aadcf2dbd83620
vinay-kaushik <vinaykaushik1111@gmail.com>
2022-02-22 19:43:00 -0600

    problem

--- salariesDifferences.sql
+++ salariesDifferences.sql
@@ -7,4 +7,4 @@ select max(de.salary) as sal2 from db_employee de,db_dept dd
     where de.department_id=dd.id 
     and dd.department='engineering')
 select mkt.sal1-eng.sal2 as salary_difference from mkt,eng
-;
+; 
