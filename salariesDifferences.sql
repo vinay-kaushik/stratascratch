f5c02010d30808faeed50ecea733f4d1b83f5d19
vinay-kaushik <vinaykaushik1111@gmail.com>
2022-02-22 19:13:34 -0600

    Solving first problem

---
+++ salariesDifferences.sql
@@ -0,0 +1,10 @@
+with mkt as (select max(de.salary) as sal1 from db_employee de,db_dept dd
+    where de.department_id=dd.id 
+    and dd.department='marketing')
+,
+eng as(
+select max(de.salary) as sal2 from db_employee de,db_dept dd
+    where de.department_id=dd.id 
+    and dd.department='engineering')
+select mkt.sal1-eng.sal2 as salary_difference from mkt,eng
+;
